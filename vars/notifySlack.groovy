35044e6de41fca9ba1569bb3fb4350a87d395c76
Karol Kurpanik <kkurpanik@gmail.com>
2022-01-03 17:09:12 +0100

    Update notifySlack

--- vars/notifySlack.groovy
+++ vars/notifySlack.groovy
@@ -1,5 +1,23 @@
 #!/usr/bin/env groovy
 
-def call(String msg = 'status is unknown.') {
-    slackSend(color: "good", message: "Your build ${msg}.")
+def call(String buildStatus = 'STARTED') {
+    buildStatus = buildStatus ?: 'SUCCESSFUL'
+
+    def colorName = 'RED'
+    def colorCode = '#FF0000'
+    def subject = "${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'"
+    def summary = "${subject} (${env.BUILD_URL})"
+
+    if (buildStatus == 'STARTED') {
+        color = 'YELLOW'
+        colorCode = '#FFFF00'
+    } else if (buildStatus == 'SUCCESSFUL') {
+        color = 'GREEN'
+        colorCode = '#00FF00'
+    } else {
+        color = 'RED'
+        colorCode = '#FF0000'
+    }
+
+    slackSend(color: "colorCode", message: summary)
 }
