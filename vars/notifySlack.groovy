06ed9a88de60e0bad4d529396e4c939b9149673d
Karol Kurpanik <kkurpanik@gmail.com>
2022-01-03 20:23:45 +0100

    Remove not needed color part

--- vars/notifySlack.groovy
+++ vars/notifySlack.groovy
@@ -3,23 +3,15 @@
 def call(String buildStatus = 'STARTED') {
     buildStatus = buildStatus ?: 'SUCCESSFUL'
 
-    def colorName = 'RED'
-    def colorCode = '#FF0000'
     def imageurl = 'https://www.jenkins.io/images/logos/fire/fire.png'
     def subject = "${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'"
     def buildurl = "${env.BUILD_URL}"
 
     if (buildStatus == 'STARTED') {
-        color = 'YELLOW'
-        colorCode = '#FFFF00'
         imageurl = 'https://www.jenkins.io/images/logos/formal/formal.png'
     } else if (buildStatus == 'SUCCESSFUL') {
-        color = 'GREEN'
-        colorCode = '#00FF00'
         imageurl = 'https://www.jenkins.io/images/logos/cute/cute.png'
     } else {
-        color = 'RED'
-        colorCode = '#FF0000'
         imageurl = 'https://www.jenkins.io/images/logos/fire/fire.png'
     }
 
@@ -45,5 +37,5 @@ def call(String buildStatus = 'STARTED') {
         ]
     ]
 
-    slackSend(color: colorCode, blocks: blocks)
+    slackSend(blocks: blocks)
 }
