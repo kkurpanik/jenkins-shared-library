3f773c7988cc55b9e44065cf3a07f21c272ae840
Karol Kurpanik <kkurpanik@gmail.com>
2022-01-04 13:53:02 +0100

    Beautify notifications

--- vars/notifySlack.groovy
+++ vars/notifySlack.groovy
@@ -4,17 +4,23 @@ def call(String buildStatus = 'STARTED') {
     buildStatus = buildStatus ?: 'SUCCESSFUL'
 
     def imageurl = 'https://www.jenkins.io/images/logos/fire/fire.png'
-    def subject = "${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'"
-    def buildurl = "${env.BUILD_URL}"
+    def msg = "Your job failed:"
+    def subject = "*${msg}\n<${env.BUILD_URL}|Build details>"
+    def section = "*Job name:*\nl${env.JOB_NAME}\n*Build number:*\n${env.BUILD_NUMBER}"
 
     if (buildStatus == 'STARTED') {
+        msg = "Your job started:"
         imageurl = 'https://www.jenkins.io/images/logos/formal/formal.png'
     } else if (buildStatus == 'SUCCESSFUL') {
+        msg= "Your job finished successfully:"
         imageurl = 'https://www.jenkins.io/images/logos/cute/cute.png'
     } else {
+        msg = "Your job failed:"
         imageurl = 'https://www.jenkins.io/images/logos/fire/fire.png'
     }
 
+    subject = "*${msg}\n<${env.BUILD_URL}|Build details>"
+    
     blocks = [
         [
             "type": "section",
@@ -27,7 +33,7 @@ def call(String buildStatus = 'STARTED') {
             "type": "section",
             "text": [
                 "type": "mrkdwn",
-                "text": buildurl
+                "text": section
             ],
             "accessory": [
                 "type": "image",
